77a06d1d6df58d54a758d340d7bae760a8ba466b
JetMuffin <564936642@qq.com>
2015-07-31 10:06:27 +0800

    修改一些Pom的依赖关系

--- src/main/java/com/Picloud/web/controller/SpaceController.java
+++ src/main/java/com/Picloud/web/controller/SpaceController.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.FileUploadException;
